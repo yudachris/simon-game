8e718aa75307aa4465ac0b135ae9d08617e06eff
Yehuda C. Nathanael <yuda.chris219@gmail.com>
2019-12-15 01:03:54 +0900

    Initial Upload
    adding files that had been built locally.

---
+++ game.js
@@ -0,0 +1,112 @@
+var titleText = $("#level-title");
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var colourPattern = [];
+
+var indexCounter = 0;
+
+var gameOver = true;
+
+$("html").on("keydown", function (e) {
+
+    if (gameOver) {
+        gameOver = false;
+        nextSequence();
+    } else {
+        //do nothing
+    }
+
+});
+
+
+$(".btn").click(function () {
+
+    var playerChosenColour = this.id;
+
+    buttonPressAnimation(playerChosenColour);
+    playSound(playerChosenColour);
+    checkPattern(playerChosenColour, indexCounter);
+});
+
+function checkPattern(color, index) {
+
+    var x = color;
+    var y = colourPattern[index];
+    var arrayLastIndex = colourPattern.length - 1;        
+
+    if(x === y && index === arrayLastIndex){
+        /*if correct and the array is on the last element (meaning the player can guess all of the sequence), 
+        adding a new color to the array and return the index counter to 0 so player need to guess the sequence from the start again. */
+        //also adding delay so the user can have some time readying themself to see the next sequence.
+        
+        setTimeout(function(){
+            nextSequence();
+        }, 1000);
+        indexCounter = 0;
+    }
+    else if(x === y){
+        //if correct, increase counter so the player can guess the next color of the array element.
+        indexCounter++;
+    }
+    else {
+        titleText.text("Game Over! Press Any Key to Restart");
+        gameOver = true;
+        indexCounter = 0;
+        colourPattern = [];
+        gameOverAnimation();
+        playSound("wrong");
+    }
+
+}
+
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColour = buttonColours[randomNumber];
+
+    colourPattern.push(randomChosenColour);
+
+    //change level text to the next level
+    titleText.text("Level " + colourPattern.length);
+    //animate button to show player which button to press next
+    nextSequenceAnimation(randomChosenColour);
+
+}
+
+
+function buttonPressAnimation(buttonColour) {
+    $("#" + buttonColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + buttonColour).removeClass("pressed");
+    }, 100);
+}
+
+function nextSequenceAnimation(randomChosenColour) {
+    $("#" + randomChosenColour).animate({
+        opacity: 0.3
+    }, 100);
+    setTimeout(function () {
+        $("#" + randomChosenColour).animate({
+            opacity: 1
+        }, 100);
+    }, 350);
+}
+
+function gameOverAnimation() {
+
+    $("body").addClass("game-over");
+    setTimeout(function () {
+        $("body").removeClass("game-over");
+    }, 50);
+
+}
+
+function playSound(buttonColour) {
+
+    var filePath = "sounds/" + buttonColour + ".mp3"
+    var audio = new Audio(filePath);
+    audio.play();
+
+}
